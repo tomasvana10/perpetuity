9498839787917fd884c88b66c87508777df0f796
tomasvana10 <tomasvana@yahoo.com>
2025-05-31 20:01:20 +1000

    add theme templates

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
 import "package:flutter/material.dart";
 import "package:perpetuity/l10n/app_localizations.dart";
+import "package:perpetuity/theme/dark_theme.dart";
+import "package:perpetuity/theme/light_theme.dart";
 
 void main() {
-  runApp(const MainApp());
+  runApp(const Perpetuity());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class Perpetuity extends StatelessWidget {
+  const Perpetuity({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: "A free, feature-rich habit tracker",
+      debugShowCheckedModeBanner: false,
+      theme: lightTheme,
+      darkTheme: darkTheme,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       home: HomePage(),
---
+++ lib/theme/light_theme.dart
@@ -0,0 +1,3 @@
+import "package:flutter/material.dart";
+
+ThemeData lightTheme = ThemeData(brightness: Brightness.light);
